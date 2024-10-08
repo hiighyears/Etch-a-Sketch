244f61b0ce5f3d201d7a525fa631c3e796193b11
Anam <121228616+hiighyears@users.noreply.github.com.>
2024-10-09 02:02:46 +0530

    sketch mechanics done

--- java.js
+++ java.js
@@ -1,28 +1,30 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click",()=> {mew = prompt("enter the number of grids");
+grid(mew);} )
+
+
+function grid(numberGrid){
+const exits = document.querySelectorAll(".sketch")
+exits.forEach((element) => element.remove());
+if (numberGrid>=5&&numberGrid<=45){
 const container = document.querySelector(".container")
 const body = document.querySelector("body")
-
-
-// const numberGrid = prompt("enter the number of grids");
-const numberGrid = 16;
+// const numberGrid = prompt("enter the number of grids")
 for(j = 0;j<numberGrid;j++){
-    const containerDiv = document.createElement("div")
+    const containerDiv = document.createElement("div");
 
 for(i = 0;i<numberGrid;i++){
-const div = document.createElement("div")
-div.style.border = "solid 2px black"
-div.style.width = "25px"
-div.style.height = "25px"
-div.className = "sketch"
-containerDiv.appendChild(div)
+const div = document.createElement("div");
+div.style.border = "solid 2px black";
+div.style.width = "25px";
+div.style.height = "25px";
+div.className = "sketch";
+div.addEventListener("mouseenter",()=>div.style.backgroundColor="black");
+containerDiv.appendChild(div);
 }
-container.append(containerDiv)
-}
-
-
-
-
-
-
-
-// containerDiv.append(div)}
-// container.append(containerDiv)}
+container.append(containerDiv);
+}}
+else{
+    alert("entered number out of range")
+}}
