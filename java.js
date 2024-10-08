2f9e2c548fe2934141a87cfcc5a2370ebead0376
Anam <121228616+hiighyears@users.noreply.github.com.>
2024-10-09 02:51:53 +0530

    finalised

--- java.js
+++ java.js
@@ -7,7 +7,7 @@ grid(mew);} )
 function grid(numberGrid){
 const exits = document.querySelectorAll(".sketch")
 exits.forEach((element) => element.remove());
-if (numberGrid>=5&&numberGrid<=45){
+if (numberGrid>=5&&numberGrid<=100){
 const container = document.querySelector(".container")
 const body = document.querySelector("body")
 // const numberGrid = prompt("enter the number of grids")
@@ -16,11 +16,25 @@ for(j = 0;j<numberGrid;j++){
 
 for(i = 0;i<numberGrid;i++){
 const div = document.createElement("div");
-div.style.border = "solid 2px black";
-div.style.width = "25px";
-div.style.height = "25px";
+div.style.border = "solid .05px black";
+div.style.width = `${700/numberGrid}px`;
+div.style.height = `${700/numberGrid}px`;
 div.className = "sketch";
-div.addEventListener("mouseenter",()=>div.style.backgroundColor="black");
+div.style.backgroundColor="white";
+div.addEventListener("mouseenter",()=>{
+ if(div.style.backgroundColor=="white"){   
+    div.style.backgroundColor=`rgb(${Math.floor(Math.random()*256)} 
+${Math.floor(Math.random()*256)} ${Math.floor(Math.random()*256)})`}
+else{
+    div.style.backgroundColor=`rgb(${Math.floor(Math.random()*256)} 
+${Math.floor(Math.random()*256)} ${Math.floor(Math.random()*256)})`
+    
+
+}
+});
+
+
+
 containerDiv.appendChild(div);
 }
 container.append(containerDiv);
